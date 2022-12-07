b06e1fa78a7f8a40901b48ab55136d287d90020c
John McCrary <johntmcc@amazon.com>
2022-12-06 21:46:31 -0600

    Jump to day 6

--- src/6.py
+++ src/6.py
@@ -1,17 +1,18 @@
+#!/bin/env python3
 
-with open('../inputs/6.txt') as fh:
-    buffer = fh.read()
-
-def uniq(chunk):
+def uniq(chunk: str) -> bool:
     return len(chunk) == len(set(chunk))
 
-def get_first_uniq_substr(buffer, uniq_length):
+def get_first_uniq_substr(buffer: str, uniq_length: int) -> int:
     buffer_length = len(buffer)
 
     for i in range(buffer_length - uniq_length):
         chunk = buffer[i:i+uniq_length]
         if uniq(chunk):
             return i + uniq_length
+
+with open('../inputs/6.txt') as fh:
+    buffer = fh.read()
 
 print('Part 1:', get_first_uniq_substr(buffer, 4))
 print('Part 2:', get_first_uniq_substr(buffer, 14))
